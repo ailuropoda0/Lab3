ee1e8d68348d1e13b17a97a2900d366e4910759e
Sung Park <sungwoo.park@students.olin.edu>
2017-11-14 21:05:20 -0500

    update cpu control signal location

--- cpu.v
+++ cpu.v
@@ -11,7 +11,7 @@ module cpu
 );
 
 //PCSel mux
-reg[1:0] PCSel;
+wire[1:0] PCSel;
 
 //Program counter
 wire[31:0] PCInput;
@@ -21,7 +21,7 @@ wire[3:0] PCLastFour;
 //AdderMux
 wire[31:0] adderMux1Out;
 wire[31:0] adderMux2Out;
-reg AdderValControl;
+wire AdderValControl;
 
 //Adder
 wire[31:0] adderOut;
@@ -46,20 +46,20 @@ assign functionCode = instrMemOut[5:0];
 //registerMux
 wire[31:0] registerMux1Out;
 wire[4:0] registerMux2Out;
-reg[1:0] RegDataWrSel;
-reg[1:0] RegAddrWrSel;
+wire[1:0] RegDataWrSel;
+wire[1:0] RegAddrWrSel;
 
 //register
 wire[31:0] regOut1;
 wire[31:0] regOut2;
-reg RegWrEn;
+wire RegWrEn;
 
 //branchControlMux
 wire branchControlOut;
-reg BranchControl;
+wire BranchControl;
 
 //alu Mux
-reg ALUImm;
+wire ALUImm;
 wire[31:0] aluMuxOut;
 
 //alu
@@ -67,148 +67,22 @@ wire[31:0] aluOut;
 wire carryout;
 wire zero;
 wire overflow;
-reg[2:0] command;
+wire[2:0] command;
 
 //Data Memory
 wire[31:0] dataMemOut;
-reg MemWrEn;
+wire MemWrEn;
 
-mux4input PCSelMux(PCInput, PCSel, {PCLastFour, jumpAddress, 2'b00}, regOut1, adderOut, adderOut);
+mux4input CSelMux(PCInput, PCSel, {PCLastFour, jumpAddress, 2'b00}, regOut1, adderOut, adderOut);
 programCounter PC(PCOutput, PCLastFour, PCInput, 1, clk);
 mux2input adderMux1(adderMux1Out, branchControlOut, instrMemImm, PCOutput);
 mux2input adderMux2(adderMux2Out, AdderValControl,  32'd4, 32'd8);
 ALU adder(.result(adderOut), .operandA(adderMux1Out), .operandB(adderMux2Out), .command(3'd0));
-instrMemory instrMem(.clk(clk), .Addr(PCOutput), .DataOut(instrMemOut), .regWE(0));
+instrMemory instrMem(.clk(clk), .Addr(PCOutput), .DataOut(instrMemOut), .regWE(0), .RegWrEn(RegWrEn), .MemWrEn(MemWrEn), .PCSel(PCSel), .AdderValControl(AdderValControl), .RegDataWrSel(RegDataWrSel), .RegAddrWrSel(RegAddrWrSel), .BranchControl(BranchControl), .ALUImm(ALUImm), .command(command));
 mux4input registerMux1(.out(registerMux1Out), .address(RegDataWrSel), .in0(aluOut), .in1(dataMemOut), .in3(adderOut));
 mux4input registerMux2(.out(registerMux2Out), .address(RegAddrWrSel), .in0(rd), .in1(rt), .in3(5'd31));
 regfile register(.ReadData1(regOut1), .ReadData2(regOut2), .WriteData(registerMux1Out), .ReadRegister1(rs), .ReadRegister2(rt), .WriteRegister(registerMux2Out), .RegWrite(RegWrEn), .Clk(clk));
 mux2input aluMux(aluMuxOut, ALUImm, regOut2, instrMemImm);
-
-initial begin
-    BranchControl = 1'b1;
-    AdderValControl = 1'b0;
-    PCSel = 2'b10;
-end
-
-always @(posedge clk) begin
-    //Decoding op code to alu operation command
-    if (opCode == 6'h23) begin //lw
-        RegWrEn <= 1'b1;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b10;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b01;
-        RegAddrWrSel <= 2'b01;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b1;
-    end
-    if (opCode == 6'h2b) begin //sw
-        RegWrEn <= 1'b0;
-        MemWrEn <= 1'b1;
-        PCSel <= 2'b10;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b01;
-        RegAddrWrSel <= 2'b01;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b1;
-    end
-    if (opCode == 6'h2) begin //j
-        RegWrEn <= 1'b0;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b00;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b01;
-        RegAddrWrSel <= 2'b01;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b1;
-    end
-    if (opCode == 6'h0 && functionCode == 6'h08) begin //jr
-        RegWrEn <= 1'b0;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b01;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b01;
-        RegAddrWrSel <= 2'b01;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b1;
-    end
-    if (opCode == 6'h3) begin //jal
-        RegWrEn <= 1'b1;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b00;
-        AdderValControl <= 1'b1;
-        RegDataWrSel <= 2'b11;
-        RegAddrWrSel <= 2'b11;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b1;
-    end
-    if (opCode == 6'd5) begin //bne
-        RegWrEn <= 1'b0;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b10;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b01;
-        RegAddrWrSel <= 2'b01;
-        BranchControl <= 1'b0;
-        ALUImm <= 1'b1;
-        command <= 3'd1;
-    end
-    if (opCode == 6'd14) begin //xori
-        command <= 3'd2;
-        RegWrEn <= 1'b1;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b10;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b00;
-        RegAddrWrSel <= 2'b01;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b1;
-    end
-    if (opCode == 6'd8) begin //addi
-        command <= 3'd0;
-        RegWrEn <= 1'b1;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b10;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b00;
-        RegAddrWrSel <= 2'b01;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b1;
-    end
-    if (opCode == 6'd0 && functionCode == 6'h20) begin //add
-        command <= 3'd0;
-        RegWrEn <= 1'b1;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b10;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b00;
-        RegAddrWrSel <= 2'b00;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b0;
-    end
-    if (opCode == 6'd0 && functionCode == 6'h22) begin //sub
-        command <= 3'd1;
-        RegWrEn <= 1'b1;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b10;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b00;
-        RegAddrWrSel <= 2'b00;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b0;
-    end
-    if (opCode == 6'd0 && functionCode == 6'h2a) begin //slt
-        command <= 3'd3;
-        RegWrEn <= 1'b1;
-        MemWrEn <= 1'b0;
-        PCSel <= 2'b10;
-        AdderValControl <= 1'b0;
-        RegDataWrSel <= 2'b00;
-        RegAddrWrSel <= 2'b00;
-        BranchControl <= 1'b1;
-        ALUImm <= 1'b0;
-    end 
-end
 
 ALU alu(.result(aluOut), .carryout(carryout), .zero(zero), .overflow(overflow), .operandA(regOut1), .operandB(aluMuxOut), .command(command));
 mux2input #(1) branchControlMux(branchControlOut, BranchControl, ~zero, 1'b1);
--- instrMemory.v
+++ instrMemory.v
@@ -4,7 +4,16 @@ module instrMemory
   input clk, regWE,
   input[9:0] Addr,
   input[31:0] DataIn,
-  output[31:0]  DataOut
+  output[31:0]  DataOut,
+    output reg RegWrEn,
+    output reg MemWrEn,
+    output reg[1:0] PCSel,
+    output reg AdderValControl,
+    output reg[1:0] RegDataWrSel,
+    output reg[1:0] RegAddrWrSel,
+    output reg BranchControl,
+    output reg ALUImm,
+    output reg[2:0] command
 );
   
   reg [31:0] mem[1023:0];  
@@ -16,6 +25,132 @@ module instrMemory
   end
   
   initial $readmemh("subTest.dat", mem);
-    
+   
+  initial begin
+    BranchControl = 1'b1;
+    AdderValControl = 1'b0;
+    PCSel = 2'b10;
+  end
+ 
   assign DataOut = mem[Addr>>2];
+
+    always @(posedge clk) begin 
+    //Decoding op code to alu operation command
+    if (DataOut[31:26] == 6'h23) begin //lw
+        RegWrEn <= 1'b1;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b10;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b01;
+        RegAddrWrSel <= 2'b01;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b1;
+    end
+    if (DataOut[31:26] == 6'h2b) begin //sw
+        RegWrEn <= 1'b0;
+        MemWrEn <= 1'b1;
+        PCSel <= 2'b10;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b01;
+        RegAddrWrSel <= 2'b01;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b1;
+    end
+    if (DataOut[31:26] == 6'h2) begin //j
+        RegWrEn <= 1'b0;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b00;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b01;
+        RegAddrWrSel <= 2'b01;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b1;
+    end
+    if (DataOut[31:26] == 6'h0 && DataOut[5:0] == 6'h08) begin //jr
+        RegWrEn <= 1'b0;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b01;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b01;
+        RegAddrWrSel <= 2'b01;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b1;
+    end
+    if (DataOut[31:26] == 6'h3) begin //jal
+        RegWrEn <= 1'b1;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b00;
+        AdderValControl <= 1'b1;
+        RegDataWrSel <= 2'b11;
+        RegAddrWrSel <= 2'b11;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b1;
+    end
+    if (DataOut[31:26] == 6'd5) begin //bne
+        RegWrEn <= 1'b0;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b10;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b01;
+        RegAddrWrSel <= 2'b01;
+        BranchControl <= 1'b0;
+        ALUImm <= 1'b1;
+        command <= 3'd1;
+    end
+    if (DataOut[31:26] == 6'd14) begin //xori
+        command <= 3'd2;
+        RegWrEn <= 1'b1;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b10;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b00;
+        RegAddrWrSel <= 2'b01;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b1;
+    end
+    if (DataOut[31:26] == 6'd8) begin //addi
+        command <= 3'd0;
+        RegWrEn <= 1'b1;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b10;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b00;
+        RegAddrWrSel <= 2'b01;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b1;
+    end
+    if (DataOut[31:26] == 6'd0 && DataOut[5:0] == 6'h20) begin //add
+        command <= 3'd0;
+        RegWrEn <= 1'b1;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b10;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b00;
+        RegAddrWrSel <= 2'b00;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b0;
+    end
+    if (DataOut[31:26] == 6'd0 && DataOut[5:0] == 6'h22) begin //sub
+        command <= 3'd1;
+        RegWrEn <= 1'b1;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b10;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b00;
+        RegAddrWrSel <= 2'b00;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b0;
+    end
+    if (DataOut[31:26] == 6'd0 && DataOut[5:0] == 6'h2a) begin //slt
+        command <= 3'd3;
+        RegWrEn <= 1'b1;
+        MemWrEn <= 1'b0;
+        PCSel <= 2'b10;
+        AdderValControl <= 1'b0;
+        RegDataWrSel <= 2'b00;
+        RegAddrWrSel <= 2'b00;
+        BranchControl <= 1'b1;
+        ALUImm <= 1'b0;
+    end 
+    end
 endmodule
